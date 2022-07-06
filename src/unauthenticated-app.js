62dfb992c34793ec6a2a833c44be1846c46f3fd9
Denis <nahuamel.denis@gmail.com>
2022-07-06 16:48:46 -0500

    create unauthenticate page

--- src/unauthenticated-app.js
+++ src/unauthenticated-app.js
@@ -1,11 +1,13 @@
 import {Navigate, Routes, Route} from "react-router-dom";
 import { LoginPage } from "./pages/login-page";
 import { SignupPage } from "./pages/signup-page";
+import UnauthenticatedPage from "./pages/unathenticated-page";
 export const UnauthenticatedApp = () => {
     return (
         <Routes>
             <Route path = "/">
-                <Route index element={<Navigate to="login" replace={true}/>}/>
+                <Route index element={<Navigate to="auth" replace={true}/>}/>
+                <Route path="auth" element={<UnauthenticatedPage />} />
                 <Route path="login" element={<LoginPage />} />
                 <Route  path= "signup" element={<SignupPage />} />
                 <Route  path = "*" element={<h2>Not Found</h2>} />
