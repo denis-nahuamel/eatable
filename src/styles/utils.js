3c4d3630fa5cc4051091cd4163b05700eda55f3e
Denis <nahuamel.denis@gmail.com>
2022-07-06 16:48:53 -0500

    add styles

---
+++ src/styles/utils.js
@@ -0,0 +1,26 @@
+export const background = `
+    background: #BCBABA;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0
+`
+
+export const container = `
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+export const containerRow = `
+    display: flex;
+    flex-direction: column;
+`
+export const buttonLogin = `
+    ${container};
+    position: absolute;
+    left: 50%;
+    right: 50%;
+    bottom: 10px;
+`
