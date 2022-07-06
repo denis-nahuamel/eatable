77db6041807e9d9aee3d70d9b181d2a03b95492c
Denis <nahuamel.denis@gmail.com>
2022-07-06 12:04:44 -0500

    Create auth-service.js

---
+++ public/services/auth-service.js
@@ -0,0 +1,13 @@
+import { tokenKey } from "../config";
+import apiFetch from "./api-fetch";
+
+export function login(credentials){
+    return apiFetch("/login", {body: credentials}).then((u)=>{
+        const {token, ...user} = u;
+        sessionStorage.setItem(tokenKey,token )
+        return user;
+    } )
+}
+export function logout(){
+    return apiFetch("/logout", {method: "DELETE"});
+}
