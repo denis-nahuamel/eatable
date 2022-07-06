b97f5713980c7bbe3c9e49e2e0620979879cb7d9
Denis <nahuamel.denis@gmail.com>
2022-07-06 14:45:44 -0500

    Fix get User

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,8 @@
 import apiFetch from "./api-fetch"
 
-export const getUser = () => {
-    const {_token, ...user} = apiFetch("profile");
-    return user;
+export function getUser() {
+    const response = apiFetch("profile");
+    console.log("response", response)
+    // const {_token, ...user} = apiFetch("profile");
+    return response;
 }
