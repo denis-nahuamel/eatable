d570db7e2a9a7a8460a2a5c769e78cc4fe3cfcf0
Denis <nahuamel.denis@gmail.com>
2022-07-07 16:51:46 -0500

    chore: update profile

--- src/components/profile-data.js
+++ src/components/profile-data.js
@@ -2,27 +2,30 @@
 import { css } from "@emotion/react";
 
 import { useAuth } from "../context/auth-context";
-import { buttonLogin, containerCard, labelInput } from "../styles/utils";
+import { buttonLogin, containerButton } from "../styles/button";
+import {  container, containerCard, containerColumn, labelInput } from "../styles/utils";
 const ProfileData = () => {
   const { user, logout } = useAuth();
   const handleLogout =()=>{
     logout().then((response)=> console.log(response));
   }
   return (
-    <div>
-      <div>Personal Details</div>
-      <div
-        css={css`
-          ${containerCard}
-        `}
-      >
-        <label>{user.name}</label>
-        <label>{user.email}</label>
-        <label>{user.phone}</label>
-        <label>{user.address}</label>
+    <div css={css`${container}`}>
+      <div css={css`${containerColumn}`}>
+        <label>Personal Details</label>
+        <div
+          css={css`
+            ${containerCard}
+          `}
+        >
+          <label>{user.name}</label>
+          <label>{user.email}</label>
+          <label>{user.phone}</label>
+          <label>{user.address}</label>
+        </div>
       </div>
-      <div css={css`${buttonLogin}`}>
-        <button onClick={handleLogout}>Logout</button>
+      <div css={css`${containerButton}`}>
+        <button css={css`${buttonLogin}`} onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
