c3ba6d10a903cceacee61d480c3597fb305d4b70
Denis <nahuamel.denis@gmail.com>
2022-07-07 16:50:11 -0500

    Create food-page.js

---
+++ src/pages/food-page.js
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useAuth } from "../context/auth-context";
+import { buttonLogin, containerButton, returnButton } from "../styles/button";
+import { cardFood, cardFoodDetail, imageFood, imageFoodDetail } from "../styles/pages";
+import { description, subTitle, title } from "../styles/typography";
+import {  containerColumn, contColumnCenter } from "../styles/utils";
+const FoodPage = () => {
+    const {addCheckoutList} = useAuth();
+    const food = JSON.parse(localStorage.getItem("current_food"));
+    const image = `
+        margin-bottom: 70px;
+    `
+
+    return (
+        <div css={css`${contColumnCenter}`}>
+            <div css={css`${image}`}>
+                <img css={css`${imageFoodDetail}`} src={food.picture_url}/>
+            </div>
+            <div css={css`${contColumnCenter}`}>
+                <label css={css`${title}`}>{food.name}</label>
+                <label css={css`${subTitle}`}>$ {food.price}</label>
+            </div>
+            <div css={css`${containerColumn}`}>
+                <label css={css`${description}`}>Description</label>
+                <label > {food.description}</label>
+            </div>
+            <div css={css`${containerButton}`}>
+                <button css={css`${buttonLogin}`} onClick={()=>addCheckoutList(food)}>Add to Cart</button>
+            </div>
+        </div>
+    )
+}
+export default FoodPage;
