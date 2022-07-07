7fafa2c57353eda9fda792c8f7ac31f9e63e8001
Denis <nahuamel.denis@gmail.com>
2022-07-06 23:25:01 -0500

    Create profile-data.js

---
+++ src/components/profile-data.js
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { containerCard, labelInput } from "../styles/utils";
+const ProfileData = () => {
+    return (
+        <div
+        css={css`
+          ${containerCard}
+        `}
+      ></div>
+    )
+}
+export default ProfileData;
