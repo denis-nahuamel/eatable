dd8a10f2970b0eaec4864cb99fe4af044754a4bc
Denis <nahuamel.denis@gmail.com>
2022-07-07 10:06:05 -0500

    get user data

--- src/components/profile-data.js
+++ src/components/profile-data.js
@@ -1,14 +1,30 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-import { containerCard, labelInput } from "../styles/utils";
+import { useAuth } from "../context/auth-context";
+import { buttonLogin, containerCard, labelInput } from "../styles/utils";
 const ProfileData = () => {
-    return (
-        <div
+  const { user, logout } = useAuth();
+  const handleLogout =()=>{
+    logout().then((response)=> console.log(response));
+  }
+  return (
+    <div>
+      <div>Personal Details</div>
+      <div
         css={css`
           ${containerCard}
         `}
-      ></div>
-    )
-}
+      >
+        <label>{user.name}</label>
+        <label>{user.email}</label>
+        <label>{user.phone}</label>
+        <label>{user.address}</label>
+      </div>
+      <div css={css`${buttonLogin}`}>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
+};
 export default ProfileData;
