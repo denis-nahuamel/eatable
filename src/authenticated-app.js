331136bb200557ee8f4197996aa9d57d121f0870
Denis <nahuamel.denis@gmail.com>
2022-07-07 16:49:25 -0500

    update: add new route

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,6 +1,7 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import { ProfilePage } from "./pages/profile-page";
 import AuthenticatedPage from "./pages/authenticated-page";
+import FoodPage from "./pages/food-page";
 export const AuthenticatedApp = () => {
     return (
         <Routes>
@@ -8,6 +9,7 @@ export const AuthenticatedApp = () => {
                 <Route index  element={<Navigate to="auth" replace={true}/>}/>
                 <Route path= "auth" element = {<AuthenticatedPage />}/>
                 <Route path="profile" element={<ProfilePage />} />
+                <Route path="auth/food" element={<FoodPage />} />
             </Route>
         </Routes>
     )
