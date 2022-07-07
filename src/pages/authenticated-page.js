f6475e5181e2b3b406f950b5dfa1b9cf79f29122
Denis <nahuamel.denis@gmail.com>
2022-07-07 16:50:59 -0500

    update handle pages

--- src/pages/authenticated-page.js
+++ src/pages/authenticated-page.js
@@ -1,31 +1,39 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { useState } from "react"
+import { listInline, listMenu } from "../styles/pages";
+import { footer } from "../styles/utils";
 import HomePage from "./home-page";
 import { ProfilePage } from "./profile-page";
 import RecordPage from "./record-page";
+import FoodPage from "./food-page";
+import CheckoutPage from "./checkout-page";
+import HeaderPage from "../components/hearder-page";
 
 const AuthenticatedPage = () => {
-   
     const [currentPage, setCurrentPage] = useState("profile");
-    const handleHome = () => {
-        setCurrentPage("home")
+    const handleFood = () => {
+        setCurrentPage("food")
     }
-    const handleProfile = () => {
-        setCurrentPage("profile")
-    }
-    const handleRecord = () => {
-        setCurrentPage("record")
+    const handleCheckout = (value) => {
+        setCurrentPage("checkout")
+        
     }
     return (
         <>
+        <HeaderPage onCurrentPage = {currentPage} onHandlePage = {handleCheckout}/>
         <div>
-            <ul>
-                <li onClick={handleHome}><img src={process.env.PUBLIC_URL + "home.png"}/></li>
-                <li onClick={handleProfile}><img src={process.env.PUBLIC_URL + "profile.png"}/></li>
-                <li onClick={handleRecord}><img src={process.env.PUBLIC_URL + "record.png"}/></li>
-            </ul>
+            {currentPage === "profile"? <ProfilePage />:
+             (currentPage === "home"? <HomePage onHandlePage = {handleFood}/>:
+             (currentPage === "food"? <FoodPage onHandleCheckout = {handleCheckout}/>:
+             (currentPage === "checkout"? <CheckoutPage/>:<RecordPage/>)) )}
         </div>
-        <div>
-            {currentPage === "profile"? <ProfilePage />: (currentPage === "home"? <HomePage/>: <RecordPage/>)}
+        <div css={css`${footer}`}>
+            <ul css={css`${listMenu}`}>
+                <li onClick={()=>setCurrentPage("home")}><img src={process.env.PUBLIC_URL + "home.png"}/></li>
+                <li onClick={()=>setCurrentPage("profile")}><img src={process.env.PUBLIC_URL + "profile.png"}/></li>
+                <li onClick={()=>setCurrentPage("record")}><img src={process.env.PUBLIC_URL + "record.png"}/></li>
+            </ul>
         </div>
         </>
     )
