e0beb73cc1cdd616acce7404887b27f8a958420f
Denis <nahuamel.denis@gmail.com>
2022-07-07 20:00:49 -0500

    Update checkout-page.js

--- src/pages/checkout-page.js
+++ src/pages/checkout-page.js
@@ -1,13 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
 import CardCheckout from "../components/card-checkout";
 import { useAuth } from "../context/auth-context";
+import { buttonLogin, containerButton } from "../styles/button";
+import { containerCheck } from "../styles/pages";
+import { containerColumn, containerGap, contColumGap } from "../styles/utils";
 
 const CheckoutPage = ()=> {
-    const {checkoutList} = useAuth()
+    const {checkoutList} = useAuth();
+    const [total, setTotal] = useState(0)
     console.log("chd", checkoutList)
+    const handleTotal = (value) => {
+        setTotal(total+ value)
+    }
     return(
-        <div>
-            {checkoutList.map((element, index)=>(<CardCheckout key={index} element= {element}/>))}
+        <>
+        <div css={css`${containerCheck}`}>
+            {checkoutList.map((element, index)=>(
+            <CardCheckout key={index} element= {element} onTotal = {handleTotal}/>)
+            )}
         </div>
+        <div>
+            <label>Total: {total}</label>
+        </div>
+        <div css={css`${containerButton}`}>
+                <button css={css`${buttonLogin}`} >Checkout</button>
+            </div>
+        </>
     )
 }
 export default CheckoutPage;
